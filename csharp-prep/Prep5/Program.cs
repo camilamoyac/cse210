2fdf41bdd2e7654c561a3b7a8e5232e76ee3946e
camilamoyac <cami.csmc@gmail.com>
2024-11-12 10:55:44 -0300

    took functions outside of main

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,21 +2,19 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {   
-        static void DisplayWelcome()
+    static void DisplayWelcome()
         {
             Console.WriteLine("Welcome to the program!");
         }
 
-        static string PromptUserName()
+    static string PromptUserName()
         {
             Console.Write("Please enter your name: ");
             string name = Console.ReadLine();
             return name;
         }
 
-        static int PromptUserNumber()
+    static int PromptUserNumber()
         {
             Console.Write("Please enter your favorite number: ");
             string numberInput = Console.ReadLine();
@@ -24,22 +22,23 @@ class Program
             return number;
         }
 
-        static int SquareNumber(int number)
+    static int SquareNumber(int number)
         {
             int squared = number * number;
             return squared;
         }
 
-        static void DisplayResult(string name, int squared)
+    static void DisplayResult(string name, int squared)
         {
             Console.WriteLine($"{name}, the square of your number is {squared}.");
         }
 
+    static void Main(string[] args)
+    {   
         DisplayWelcome();
         string name = PromptUserName();
         int number = PromptUserNumber();
         int squared = SquareNumber(number);
         DisplayResult(name, squared);
-        
     }
 }
