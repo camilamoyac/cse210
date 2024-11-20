f0da1fd92f5774241f6abb79f8b5ed1203f7193f
camilamoyac <cami.csmc@gmail.com>
2024-11-20 18:43:24 -0300

    finished program

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -6,6 +6,16 @@ public class Entry
     public string _prompt;
     public string _entry;
 
+    public Entry(string date, string prompt, string entry)
+    {
+        _date = date;
+        _prompt = prompt;
+        _entry = entry;
+    }
+
     public void DisplayOne()
-    {}
+    {
+        Console.WriteLine($"Date: {_date} - Prompt: {_prompt}");
+        Console.WriteLine($"{_entry}\n");
+    }
 }
--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -1,11 +1,17 @@
 using System;
+using System.Collections.Generic;
 
 public class Prompt
 {
-    public List<string> _promptList;
+    public List<string> _promptList = new List<string> {"What am I grateful for today?",
+    "What is one good thing and one bad thing that happened today?", "What did I learn today?",
+    "What surprised me today?", "What am i looking forward to tomorrow?", "What reminded me of God today?",
+    "What made me smile today?", "What did I accomplish today?", "What's something you want to remember from today?"};
 
     public string GivePrompt()
     {
-        return "";
+        Random random = new Random();
+        int randomIndex = random.Next(_promptList.Count);
+        return _promptList[randomIndex];
     }
 }
