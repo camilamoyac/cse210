0f43cf5b16dc4127a9b2786aa5919d437be98af8
camilamoyac <cami.csmc@gmail.com>
2024-12-04 10:49:53 -0300

    finished program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment1 = new Assignment("Camila Moya", "Science");
+        Console.WriteLine(assignment1.getSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Joe Mama", "Addition", "5.2", "1-20");
+        Console.WriteLine(assignment2.getSummary());
+        Console.WriteLine(assignment2.getHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Buzz Lightyear", "IIWW", "Why the Nazis were Bad");
+        Console.WriteLine(assignment3.getSummary());
+        Console.WriteLine(assignment3.getWritingInformation());
     }
 }
