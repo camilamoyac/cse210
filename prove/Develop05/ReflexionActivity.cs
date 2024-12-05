3d3d761e467571289cf225a783ce21fc161f80f3
camilamoyac <cami.csmc@gmail.com>
2024-12-05 09:00:18 -0300

    class skeletons

---
+++ prove/Develop05/ReflexionActivity.cs
@@ -0,0 +1,33 @@
+using System;
+
+public class ReflexionActivity : Activity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+
+    public ReflexionActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+    public string GetRandomQuestion()
+    {
+        return "";
+    }
+    public void DisplayPrompt()
+    {
+
+    }
+    public void DisplayQuestions()
+    {
+
+    }
+}
