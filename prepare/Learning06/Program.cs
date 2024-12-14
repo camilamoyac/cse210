b57d8ab700165e0c3e28083ba6e8b6c2c94c12de
camilamoyac <cami.csmc@gmail.com>
2024-12-14 08:54:53 -0300

    finished

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("red", 3.2);
+        shapes.Add(square);
+
+        Circle circle = new Circle("yellow", 3.2);
+        shapes.Add(circle);
+
+        Rectangle rectangle = new Rectangle("blue", 3.2, 5.6);
+        shapes.Add(rectangle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area:f2}.");
+        }
     }
 }
---
+++ prepare/Learning06/Rectangle.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Rectangle : Shape
+{
+    private double _side1;
+    private double _side2;
+
+    public Rectangle(string color, double side1, double side2) : base(color)
+    {
+        _side1 = side1;
+        _side2 = side2;
+    }
+    public override double GetArea()
+    {
+        return (double)_side1 * _side2;
+    }
+}
